b3790f12a07bbe5ad2ef898eefce9f7d7f2cc793
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-12-01 07:21:39 +0100

    Solve day01/part02

--- src/day01/calories.ts
+++ src/day01/calories.ts
@@ -3,18 +3,23 @@ import { getInputStrings } from "../utils.ts";
 const getElfInventories = (inputLines: string[]) =>
   inputLines.reduce(
     ([head, ...tail], line) =>
-      line.length === 0
-        ? [[], head, ...tail]
-        : [[...head, parseInt(line)], ...tail],
-    [[]] as number[][],
+      line.length === 0 ? [0, head, ...tail] : [head + parseInt(line), ...tail],
+    [0],
   );
 
 export const solvePart1 = (filePath: string) => {
   const elves = getElfInventories(getInputStrings(filePath));
 
   return elves.reduce(
-    (greatest, elf) =>
-      Math.max(greatest, elf.reduce((cals, meal) => cals + meal, 0)),
+    (greatest, elf) => Math.max(greatest, elf),
     -Infinity,
+  );
+};
+
+export const solvePart2 = (filePath: string) => {
+  const elves = getElfInventories(getInputStrings(filePath));
+
+  return elves.sort((a, b) => b - a).slice(0, 3).reduce((acc, curr) =>
+    acc + curr
   );
 };
--- src/day01/test.ts
+++ src/day01/test.ts
@@ -1,9 +1,15 @@
 import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
-import { solvePart1 } from "./calories.ts";
+import { solvePart1, solvePart2 } from "./calories.ts";
 
 Deno.test("part1 testinput", () => {
   assertEquals(solvePart1("src/day01/testinput.txt"), 24000);
 });
 Deno.test("part1 realinput", () => {
   assertEquals(solvePart1("src/day01/input.txt"), 70698);
+});
+Deno.test("part2 testinput", () => {
+  assertEquals(solvePart2("src/day01/testinput.txt"), 45000);
+});
+Deno.test("part2 realinput", () => {
+  assertEquals(solvePart2("src/day01/input.txt"), 206643);
 });
