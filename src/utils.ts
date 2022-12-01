e3e8e0d2bafebd61427fa1a68485d4471c3938fc
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-12-01 08:47:53 +0100

    More robust newline detection

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
 import { readFileSync } from "fs";
 
 export const getInputStrings = (filePath: string) =>
-	readFileSync(filePath, { encoding: "utf-8" }).split(/\r\n/);
+	readFileSync(filePath, { encoding: "utf-8" })
+		.replace(/(\r\n)|\r|\n/g, "\n")
+		.split(/\n/g);
