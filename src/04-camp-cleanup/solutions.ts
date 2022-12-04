bc9a1720fcd0f23b8e3276ff945b8fd2c5e7f73a
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-12-04 11:24:51 +0100

    Comment day04 functions

--- src/04-camp-cleanup/solutions.ts
+++ src/04-camp-cleanup/solutions.ts
@@ -3,26 +3,41 @@ import { getInputStrings } from "../utils";
 type Range = [number, number];
 type RangePair = [Range, Range];
 
-const isPair = <T>(nums: T[]): nums is [T, T] => nums.length === 2;
+/**
+ * A little utility to narrow from an array to a tuple of length 2
+ */
+const isPair = <T>(elements: T[]): elements is [T, T] => elements.length === 2;
 
+/**
+ * Parse strings of the form "3-42" to Range
+ */
 const toRange = (instruction: string): Range => {
 	const bounds = instruction.split("-").map((str) => parseInt(str));
 	if (isPair(bounds)) return bounds;
 	throw new Error(`Unrecognised instruction: ${instruction}`);
 };
 
+/**
+ * Parse strings of the form "1-12,3-42" to RangePair
+ */
 const toRangePair = (line: string): RangePair => {
 	const nums = line.split(",").map(toRange);
 	if (isPair(nums)) return nums;
 	throw new Error(`Unrecognised line: ${line}`);
 };
 
+/**
+ * Returns true if either range fully overlaps the other
+ */
 const fullyOverlaps = ([[aStart, aEnd], [bStart, bEnd]]: RangePair): boolean =>
 	(aStart <= bStart && aEnd >= bEnd) || (bStart <= aStart && bEnd >= aEnd);
 
 export const solvePart1 = (filePath: string) =>
 	getInputStrings(filePath).map(toRangePair).filter(fullyOverlaps).length;
 
+/**
+ * Returns true if there is any overlap at all between the two ranges
+ */
 const partiallyOverlaps = ([
 	[aStart, aEnd],
 	[bStart, bEnd],
