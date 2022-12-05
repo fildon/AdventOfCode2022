9deaf08219d2666bf28ca2c7ad79ea80eef334be
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-12-05 12:05:13 +0100

    Use dirname in tests for convenience

--- src/02-rock-paper-scissors/solutions.test.ts
+++ src/02-rock-paper-scissors/solutions.test.ts
@@ -1,14 +1,14 @@
 import { solvePart1, solvePart2 } from "./solutions";
 
 test("part1 test input", () => {
-	expect(solvePart1("src/02-rock-paper-scissors/input-test.txt")).toBe(15);
+	expect(solvePart1(`${__dirname}/input-test.txt`)).toBe(15);
 });
 test("part1 real input", () => {
-	expect(solvePart1("src/02-rock-paper-scissors/input-real.txt")).toBe(14069);
+	expect(solvePart1(`${__dirname}/input-real.txt`)).toBe(14069);
 });
 test("part2 test input", () => {
-	expect(solvePart2("src/02-rock-paper-scissors/input-test.txt")).toBe(12);
+	expect(solvePart2(`${__dirname}/input-test.txt`)).toBe(12);
 });
 test("part2 real input", () => {
-	expect(solvePart2("src/02-rock-paper-scissors/input-real.txt")).toBe(12411);
+	expect(solvePart2(`${__dirname}/input-real.txt`)).toBe(12411);
 });
