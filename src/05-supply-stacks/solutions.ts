384a337b25748effac7de3dbab06a03569309d8b
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-12-05 12:05:00 +0100

    Refactor day05

--- src/05-supply-stacks/solutions.ts
+++ src/05-supply-stacks/solutions.ts
@@ -1,4 +1,4 @@
-import { getInputStrings } from "../utils";
+import { getInput } from "../utils";
 
 type Stacks = Array<Array<string>>;
 type Instruction = {
@@ -7,44 +7,38 @@ type Instruction = {
 	to: number;
 };
 
-const parse = (
-	lines: string[]
+const parseFile = (
+	filePath: string
 ): { stacks: Stacks; instructions: Instruction[] } => {
-	const inputWidth = lines[0].length;
+	const [stacksSection, instructionLines] = getInput(filePath)
+		.split("\n\n")
+		.map((section) => section.split("\n"));
+
+	// Slice off the last line which is the stack column labels
+	const stackLines = stacksSection.slice(0, stacksSection.length - 1);
+
+	const inputWidth = stackLines[0].length;
 	const stackCount = 1 + (inputWidth - 3) / 4;
 	const stacks: Stacks = Array.from({ length: stackCount }).map(() => []);
 
-	let inputStage = 1;
-	let linePointer = 0;
-	while (inputStage === 1) {
-		const currentLine = lines[linePointer];
-		// Detect the end of the first input stage
-		if (currentLine.slice(0, 2) === " 1") {
-			// Skip to start of next section
-			linePointer += 2;
-			inputStage = 2;
-			break;
-		} else {
-			currentLine
-				.split("")
-				// Fetch only the relevant input columns
-				.filter((_, i) => (i - 1) % 4 === 0)
-				// Push only the non-empty items to their columns
-				.forEach((char, i) => char !== " " && stacks[i].push(char));
-		}
+	stackLines.forEach((line) =>
+		line
+			.split("")
+			// Fetch only the relevant input columns
+			.filter((_, i) => (i - 1) % 4 === 0)
+			// Push only the non-empty items to their columns
+			.forEach((char, i) => char !== " " && stacks[i].push(char))
+	);
 
-		linePointer++;
-	}
-
-	const instructions: Instruction[] = [];
-	for (let i = linePointer; i < lines.length; i++) {
-		const [qty, from, to] = lines[i]
-			.split(" ")
-			.filter((_, i) => i % 2 === 1)
-			.map((val) => parseInt(val));
-		// -1 to convert from 1-index to 0-index
-		instructions.push({ qty, from: from - 1, to: to - 1 });
-	}
+	const instructions = instructionLines
+		.map((line) =>
+			line
+				.split(" ")
+				.filter((_, i) => i % 2 === 1)
+				.map((val) => parseInt(val))
+		)
+		// -1 to transform from 1-index to 0-index
+		.map(([qty, from, to]) => ({ qty, from: from - 1, to: to - 1 }));
 
 	return { stacks, instructions };
 };
@@ -56,23 +50,19 @@ const executeInstruction =
 			? stacks[from].slice(0, qty)
 			: stacks[from].slice(0, qty).reverse();
 
-		const newStacks = stacks.map((stack, i) => {
-			if (i === from) {
-				return stack.slice(qty); // Remove the moving slice from the from column
-			}
-			if (i === to) {
-				return [...movingSlice, ...stack]; // Push moving slice on top of the stack
-			}
-			return stack;
-		});
-
-		return newStacks;
+		return stacks.map((stack, i) =>
+			i === from
+				? stack.slice(qty) // Remove the moving slice from the from column
+				: i === to
+				? [...movingSlice, ...stack] // Push moving slice on top of the stack
+				: stack
+		);
 	};
 
 const solve =
 	({ withMultiMove }: { withMultiMove: boolean }) =>
 	(filePath: string) => {
-		const { stacks, instructions } = parse(getInputStrings(filePath));
+		const { stacks, instructions } = parseFile(filePath);
 
 		return instructions
 			.reduce(executeInstruction({ withMultiMove }), stacks)
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from "fs";
 
-export const getInputStrings = (filePath: string) =>
-	readFileSync(filePath, { encoding: "utf-8" })
-		.replace(/(\r\n)|\r|\n/g, "\n")
-		.split(/\n/g);
+export const getInput = (filePath: string): string =>
+	readFileSync(filePath, { encoding: "utf-8" }).replace(/(\r\n)|\r|\n/g, "\n");
+
+export const getInputStrings = (filePath: string): string[] =>
+	getInput(filePath).split(/\n/g);
