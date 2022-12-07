b9ef721e3bc2deafc9833605ed755302657b9f7f
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-12-07 09:09:01 +0100

    Solves day06 part 2

--- src/06-tuning-trouble/solutions.test.ts
+++ src/06-tuning-trouble/solutions.test.ts
@@ -1,4 +1,4 @@
-import { solvePart1 } from "./solutions";
+import { solvePart1, solvePart2 } from "./solutions";
 
 test("part1 test input", () => {
 	expect(solvePart1(`${__dirname}/input-test.txt`)).toBe(7);
@@ -6,9 +6,9 @@ test("part1 test input", () => {
 test("part1 real input", () => {
 	expect(solvePart1(`${__dirname}/input-real.txt`)).toBe(1142);
 });
-// test("part2 test input", () => {
-// 	expect(solvePart2(`${__dirname}/input-test.txt`)).toBe(-1);
-// });
-// test("part2 real input", () => {
-// 	expect(solvePart2(`${__dirname}/input-real.txt`)).toBe(-1);
-// });
+test("part2 test input", () => {
+	expect(solvePart2(`${__dirname}/input-test.txt`)).toBe(19);
+});
+test("part2 real input", () => {
+	expect(solvePart2(`${__dirname}/input-real.txt`)).toBe(2803);
+});
--- src/06-tuning-trouble/solutions.ts
+++ src/06-tuning-trouble/solutions.ts
@@ -20,3 +20,34 @@ export const solvePart1 = (filePath: string): number => {
 
 	throw new Error("No marker found");
 };
+
+export const solvePart2 = (filePath: string): number => {
+	const datastream = getInput(filePath);
+
+	for (let i = 13; i < datastream.length; i++) {
+		if (
+			[
+				...new Set([
+					datastream[i - 13],
+					datastream[i - 12],
+					datastream[i - 11],
+					datastream[i - 10],
+					datastream[i - 9],
+					datastream[i - 8],
+					datastream[i - 7],
+					datastream[i - 6],
+					datastream[i - 5],
+					datastream[i - 4],
+					datastream[i - 3],
+					datastream[i - 2],
+					datastream[i - 1],
+					datastream[i],
+				]).values(),
+			].length === 14
+		) {
+			return i + 1;
+		}
+	}
+
+	throw new Error("No marker found");
+};
