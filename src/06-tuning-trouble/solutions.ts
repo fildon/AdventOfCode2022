28a8916e4d2b423dfb6d534f62a73e1609a72b1c
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-12-07 09:10:27 +0100

    Minor refactor

--- src/06-tuning-trouble/solutions.ts
+++ src/06-tuning-trouble/solutions.ts
@@ -5,14 +5,12 @@ export const solvePart1 = (filePath: string): number => {
 
 	for (let i = 3; i < datastream.length; i++) {
 		if (
-			[
-				...new Set([
-					datastream[i - 3],
-					datastream[i - 2],
-					datastream[i - 1],
-					datastream[i],
-				]).values(),
-			].length === 4
+			new Set([
+				datastream[i - 3],
+				datastream[i - 2],
+				datastream[i - 1],
+				datastream[i],
+			]).size === 4
 		) {
 			return i + 1;
 		}
@@ -26,24 +24,22 @@ export const solvePart2 = (filePath: string): number => {
 
 	for (let i = 13; i < datastream.length; i++) {
 		if (
-			[
-				...new Set([
-					datastream[i - 13],
-					datastream[i - 12],
-					datastream[i - 11],
-					datastream[i - 10],
-					datastream[i - 9],
-					datastream[i - 8],
-					datastream[i - 7],
-					datastream[i - 6],
-					datastream[i - 5],
-					datastream[i - 4],
-					datastream[i - 3],
-					datastream[i - 2],
-					datastream[i - 1],
-					datastream[i],
-				]).values(),
-			].length === 14
+			new Set([
+				datastream[i - 13],
+				datastream[i - 12],
+				datastream[i - 11],
+				datastream[i - 10],
+				datastream[i - 9],
+				datastream[i - 8],
+				datastream[i - 7],
+				datastream[i - 6],
+				datastream[i - 5],
+				datastream[i - 4],
+				datastream[i - 3],
+				datastream[i - 2],
+				datastream[i - 1],
+				datastream[i],
+			]).size === 14
 		) {
 			return i + 1;
 		}
