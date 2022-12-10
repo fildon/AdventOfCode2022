fb85626ace0e84a5e4a246af2c62d8e3620fb07c
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-12-10 07:02:57 +0100

    Solves day10 part 2

--- src/10-cathode-ray-tube/solutions.ts
+++ src/10-cathode-ray-tube/solutions.ts
@@ -14,9 +14,8 @@ const parseInputLine = (inputLine: string): Command =>
 		? { type: "noop" }
 		: { type: "addx", value: parseInt(inputLine.split(" ")[1]) };
 
-export const solvePart1 = (filePath: string) => {
-	const commands = getInputStrings(filePath).map(parseInputLine);
-	const registerValues = commands.reduce(
+const computeRegisterValues = (commands: Array<Command>): Array<number> =>
+	commands.reduce(
 		(signals, command) => {
 			const lastSignal = signals.at(-1);
 			if (lastSignal === undefined) throw new Error(`Lost signal`);
@@ -29,7 +28,29 @@ export const solvePart1 = (filePath: string) => {
 		},
 		[1]
 	);
+
+export const solvePart1 = (filePath: string) => {
+	const registerValues = computeRegisterValues(
+		getInputStrings(filePath).map(parseInputLine)
+	);
 	const signalStrengths = registerValues.map((x, i) => x * (i + 1));
 	const interestingSignals = signalStrengths.filter((_, i) => i % 40 === 19);
 	return interestingSignals.reduce(sum, 0);
+};
+
+export const solvePart2 = (filePath: string): string => {
+	const registerValues = computeRegisterValues(
+		getInputStrings(filePath).map(parseInputLine)
+	);
+
+	return Array.from({ length: 240 })
+		.flatMap((_, cycleIndex) => {
+			const xPos = cycleIndex % 40;
+			const spritePos = registerValues[cycleIndex];
+			const renderedPixel = Math.abs(spritePos - xPos) <= 1 ? "█" : " ";
+			return `${
+				cycleIndex > 0 && cycleIndex % 40 === 0 ? "\n" : ""
+			}${renderedPixel}`;
+		})
+		.join("");
 };
