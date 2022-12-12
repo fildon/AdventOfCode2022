c77095122b3a37dfc882d946a419824da26c85ac
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-12-12 20:58:38 +0100

    Solves day12 part 2

--- src/12-hill-climbing-algorithm/solutions.ts
+++ src/12-hill-climbing-algorithm/solutions.ts
@@ -68,3 +68,43 @@ export const solvePart1 = (filePath: string) => {
 	}
 	return steps;
 };
+export const solvePart2 = (filePath: string) => {
+	const { end, heightMap } = parseInput(getInputStrings(filePath));
+	const visited = new Set<string>([serialize(end)]);
+	let frontier: Array<Coordinate> = [end];
+	let steps = 0;
+
+	while (
+		![...visited.values()]
+			.map(deserialize)
+			.map(([row, col]) => heightMap[row][col])
+			.includes("a".charCodeAt(0))
+	) {
+		steps++;
+		const newFrontier = frontier.flatMap(([row, col]) =>
+			[
+				[row, col + 1] as Coordinate,
+				[row, col - 1] as Coordinate,
+				[row + 1, col] as Coordinate,
+				[row - 1, col] as Coordinate,
+			].filter(([newRow, newCol]) => {
+				// Target is out of bounds
+				if (
+					newRow < 0 ||
+					newRow >= heightMap.length ||
+					newCol < 0 ||
+					newCol >= heightMap[0].length
+				)
+					return false;
+				// Target is too low
+				if (heightMap[newRow][newCol] < heightMap[row][col] - 1) return false;
+				// Target has already been visited
+				if (visited.has(`${newRow},${newCol}`)) return false;
+				return true;
+			})
+		);
+		frontier = dedupe(newFrontier);
+		newFrontier.map(serialize).forEach((x) => visited.add(x));
+	}
+	return steps;
+};
