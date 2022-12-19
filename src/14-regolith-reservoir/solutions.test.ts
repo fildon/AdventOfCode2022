dae9b805cc6e13b37f3019d45fc9ea4cd5a1977e
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-12-19 11:21:38 +0100

    Day14 solves part 1

--- src/14-regolith-reservoir/solutions.test.ts
+++ src/14-regolith-reservoir/solutions.test.ts
@@ -1,8 +1,8 @@
 import { solvePart1 } from "./solutions";
 
 test("part1 test input", () => {
-	expect(solvePart1(`${__dirname}/input-test.txt`, { debug: true })).toBe(NaN);
+	expect(solvePart1(`${__dirname}/input-test.txt`, { debug: false })).toBe(24);
 });
-test.skip("part1 real input", () => {
-	expect(solvePart1(`${__dirname}/input-real.txt`)).toBe(NaN);
+test("part1 real input", () => {
+	expect(solvePart1(`${__dirname}/input-real.txt`)).toBe(873);
 });
--- src/14-regolith-reservoir/solutions.ts
+++ src/14-regolith-reservoir/solutions.ts
@@ -1,22 +1,30 @@
 import { getInputStrings, max, min, range } from "../utils";
 
 type Vector = `${string},${string}`;
+type NumericVector = [number, number];
 
 type Wall = Set<Vector>;
+
+const toNumericVector = (vector: Vector) =>
+	vector.split(",").map((v) => parseInt(v)) as NumericVector;
 
 const add =
 	(a: Vector) =>
 	(b: Vector): Vector => {
-		const [aX, aY] = a.split(",").map((val) => parseInt(val));
-		const [bX, bY] = b.split(",").map((val) => parseInt(val));
+		const [aX, aY] = toNumericVector(a);
+		const [bX, bY] = toNumericVector(b);
 		return `${aX + bX},${aY + bY}`;
 	};
+
+const down = add("0,1");
+const downL = add("-1,1");
+const downR = add("1,1");
 
 const merge = (a: Wall, b: Wall): Wall => new Set([...a, ...b]);
 
 const buildWall = ([start, end]: [Vector, Vector]): Wall => {
-	const [startX, startY] = start.split(",").map((c) => parseInt(c));
-	const [endX, endY] = end.split(",").map((c) => parseInt(c));
+	const [startX, startY] = toNumericVector(start);
+	const [endX, endY] = toNumericVector(end);
 
 	const step = add(
 		startX < endX
@@ -46,13 +54,11 @@ const parseInputLine = (inputLine: string): Wall => {
 };
 
 const toString = (wall: Wall): string => {
-	const numericVectors = [...wall].map(
-		(vec) => vec.split(",").map((v) => parseInt(v)) as [number, number]
-	);
-	const maxX = numericVectors.map(([x, _]) => x).reduce(max);
-	const minX = numericVectors.map(([x, _]) => x).reduce(min);
-	const maxY = numericVectors.map(([_, y]) => y).reduce(max);
-	const minY = numericVectors.map(([_, y]) => y).reduce(min);
+	const numericVectors = [...wall].map(toNumericVector);
+	const maxX = numericVectors.map(([x]) => x).reduce(max);
+	const minX = numericVectors.map(([x]) => x).reduce(min);
+	const maxY = numericVectors.map(([, y]) => y).reduce(max);
+	const minY = numericVectors.map(([, y]) => y).reduce(min);
 
 	return range(minY - 1, maxY + 1)
 		.map((y) =>
@@ -61,6 +67,12 @@ const toString = (wall: Wall): string => {
 				.join("")
 		)
 		.join("\n");
+};
+
+const hasDescendedIntoVoid = (wall: Wall, vec?: Vector) => {
+	if (!vec) return false;
+	const [, vecY] = toNumericVector(vec);
+	return [...wall].map(toNumericVector).every(([, wallY]) => wallY < vecY);
 };
 
 export const solvePart1 = (
@@ -75,5 +87,31 @@ export const solvePart1 = (
 		console.log(toString(walls));
 	}
 
-	return -1;
+	let sandCount = 0;
+	const fallingPath: Array<Vector> = [];
+
+	while (!hasDescendedIntoVoid(walls, fallingPath.at(-1))) {
+		const currentPosition = fallingPath.at(-1) ?? "500,0";
+
+		// Try to descend in each of the three downward directions
+		if (!walls.has(down(currentPosition))) {
+			fallingPath.push(down(currentPosition));
+		} else if (!walls.has(downL(currentPosition))) {
+			fallingPath.push(downL(currentPosition));
+		} else if (!walls.has(downR(currentPosition))) {
+			fallingPath.push(downR(currentPosition));
+		} else {
+			// Nowhere to go, so settle
+			walls.add(currentPosition);
+			// The next grain will resume from one step prior on the path
+			fallingPath.pop();
+			sandCount++;
+
+			if (debug) {
+				console.log(toString(walls));
+			}
+		}
+	}
+
+	return sandCount;
 };
